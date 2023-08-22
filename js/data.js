d211104ed30e49830b47e2aecbb68c343f65ba30
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-21 22:09:50 -0300

    Cambios

--- js/data.js
+++ js/data.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
     let data = document.getElementById("data");
     let array = localStorage.getItem("array");
-    let stringElement = JSON.stringify(array);
-    if(stringElement == "null"){
-        data.innerHTML = "No se ha introducido ningún dato";
-    } else {
-        data.innerHTML += stringElement;
-    }
+    data.innerHTML += array;
 });
